f29aa914d48c1e2cb2271a76fc4e8fd79bc3a4fc
Shailesh <shailesh.shanbhags@gmail.com>
2025-06-11 12:38:00 +0530

    animation added

--- progressbar/app/components/progressbar.js
+++ progressbar/app/components/progressbar.js
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react";
+
 export default function ProgressBar({ progress }) {
+
+  const [animatedProgress, setAnimatedProgress] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAnimatedProgress(progress);
+    }, 100);
+  }, [progress])
+
   return (
     <div className="outer">
-      <div className="inner" style={{ width: `${progress}%` }}>
-        {progress}%
+      <div className="inner" style={{ /* transform: `translateX(${animatedProgress - 100}%)` ,*/width: `${animatedProgress}%`, color: (progress <= 0) ? 'black' : 'white' }}>
+        {animatedProgress}%
       </div>
     </div >
   );
