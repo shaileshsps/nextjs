c1b1ffdb5e4a4a40d44edd6c90ca7106aa1b01c7
Shailesh <shailesh.shanbhags@gmail.com>
2025-06-14 18:03:49 +0530

    accordian

---
+++ user-interfaces/app/components/accordion.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+
+function Accordion({ items }) {
+  const [openIndex, setOpenIndex] = useState(null);
+
+  const handleClick = (index) => {
+    setOpenIndex(openIndex === index ? null : index);
+  }
+
+  return items.length == 0 ? (
+    <div className="accordion">No records found</div>
+  ) : (
+    <div className="accordion">
+      {items.map((item, index) => {
+        return (
+          <div key={index} className="accordion-item">
+            <div className="accordion-title" onClick={() => handleClick(index)}>
+              <p>{item.title} {openIndex === index ? <FaChevronUp className="right" /> : <FaChevronDown className="right" />}</p>
+            </div>
+            {openIndex === index && <div className="accordion-content">
+              <p>{item.content}</p>
+            </div>}
+          </div>
+        )
+      })}
+    </div>
+  );
+}
+
+export default Accordion;
--- user-interfaces/app/page.js
+++ user-interfaces/app/page.js
@@ -1,12 +1,33 @@
 'use client';
 import ProgressBar from "./components/progressbar";
+import Accordion from "./components/accordion";
 import "./style.css";
 
 const bars = [0, 5, 10, 35, 50, 75, 100];
 
 export default function Home() {
+  const accordianItems = [
+    {
+      title: "JavaScript Basics",
+      content: "Learn variables, functions, and loops in JavaScript."
+    },
+    {
+      title: "React.js Overview",
+      content: "Understand components, state, and props in React."
+    },
+    {
+      title: "Node.js",
+      content: "Basics of server-side development with Node.js."
+    },
+    {
+      title: "Full-Stack Development",
+      content: "Build full-stack apps with React and Node.js."
+    },
+  ];
+
   return (
     <>
+      {/* Progress Bar */}
       <div style={{ textAlign: 'center' }}>
         <h1>Progress Bar</h1>
         <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
@@ -16,6 +37,11 @@ export default function Home() {
             ))
           }
         </div>
+      </div>
+      {/* Accordion */}
+      <div style={{ textAlign: 'center' }}>
+        <h1>Accordion</h1>
+        <Accordion items={accordianItems} />
       </div>
     </>
   );
