1df0a99c40fd85dbf27a72a02637c25de8bb73c9
Shailesh <shailesh.shanbhags@gmail.com>
2025-06-16 00:45:10 +0530

    chip input

--- user-interfaces/app/components/chips-input.js
+++ user-interfaces/app/components/chips-input.js
@@ -1,15 +1,41 @@
 import React, { useState } from "react";
-import './styles.css'
 function ChipsInput() {
+
+  const [inputText, setinputText] = useState("");
+  const [chips, setChips] = useState([]);
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && inputText.trim() !== "") {
+      // setChips([...chips, inputText])
+      setChips(prev => [...prev, inputText])
+      setinputText("");
+    }
+  }
+
+  const handleDeleteClick = (index) => {
+    const chipsCopy = [...chips];
+    chipsCopy.splice(index, 1);
+    setChips(chipsCopy)
+  }
 
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", margin: "40px 0" }}>
-      <h2>Chips Input</h2>
       <input
         type="text"
         placeholder="Type a chip and press tag"
         style={{ padding: "8px", width: "200px" }}
+        value={inputText}
+        onChange={(e) => setinputText(e.target.value)}
+        onKeyDown={(e) => handleKeyDown(e)}
       />
+      <div style={{ display: "flex" }}>
+        {chips.map((chip, index) => (
+          <div key={index} style={{ background: "grey", width: "130px", height: "50px", borderRadius: "10px", color: "white", margin: "10px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+            {chip}
+            <button style={{ margin: "0 20px", float: "right" }} onClick={() => handleDeleteClick(index)}>X</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
