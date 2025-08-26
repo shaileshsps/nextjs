a93caf794bcc2c34631d6062f8bfbf550269be5f
Shailesh <shailesh.shanbhags@gmail.com>
2025-08-26 22:50:01 +0530

    nested checkbox user handled

--- user-interfaces/app/components/nested-checkboxes.js
+++ user-interfaces/app/components/nested-checkboxes.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 export default function Checkboxes({ data }) {
+
+  const [checked, setChecked] = useState({});
+
+  const handleOnChange = (isChecked, node) => {
+    setChecked((prev) => {
+      const newState = { ...prev, [node.id]: isChecked }
+      return newState;
+    })
+  }
+
   return (
-    <div style={{ textAlign: "left" }}>
+    <div>
       {
         data.map((node) => (
           <div key={node.id} className="parent-checkbox-container">
-            <input type="checkbox" />
+            <input type="checkbox"
+              checked={checked[node.id] || false}
+              onChange={(e) => handleOnChange(e.target.checked, node)}
+            />
             {node.label}
             {node.children && <Checkboxes data={node.children} />}
           </div>
