68b3203935ca54e9d1bcfef38a05b2ca238ccd12
Shailesh <shailesh.shanbhags@gmail.com>
2025-08-31 23:48:44 +0530

    accordian code organised

--- user-interfaces/app/components/accordion.js
+++ user-interfaces/app/components/accordion.js
@@ -1,18 +1,37 @@
 import React, { useState } from "react";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 
-function Accordion({ items }) {
+const accordianItems = [
+  {
+    title: "JavaScript Basics",
+    content: "Learn variables, functions, and loops in JavaScript."
+  },
+  {
+    title: "React.js Overview",
+    content: "Understand components, state, and props in React."
+  },
+  {
+    title: "Node.js",
+    content: "Basics of server-side development with Node.js."
+  },
+  {
+    title: "Full-Stack Development",
+    content: "Build full-stack apps with React and Node.js."
+  },
+];
+
+function Accordion() {
   const [openIndex, setOpenIndex] = useState(null);
 
   const handleClick = (index) => {
     setOpenIndex(openIndex === index ? null : index);
   }
 
-  return items.length == 0 ? (
+  return accordianItems.length == 0 ? (
     <div className="accordion">No records found</div>
   ) : (
     <div className="accordion">
-      {items.map((item, index) => {
+      {accordianItems.map((item, index) => {
         return (
           <div key={index} className="accordion-item">
             <div className="accordion-title" style={{ backgroundColor: (openIndex === index) ? "rgb(224, 224, 224)" : '' }} onClick={() => handleClick(index)}>
--- user-interfaces/app/page.js
+++ user-interfaces/app/page.js
@@ -8,24 +8,6 @@ import NestedCheckboxes from "./components/nested-checkboxes";
 const bars = [0, 5, 10, 35, 50, 75, 100];
 
 export default function Home() {
-  const accordianItems = [
-    {
-      title: "JavaScript Basics",
-      content: "Learn variables, functions, and loops in JavaScript."
-    },
-    {
-      title: "React.js Overview",
-      content: "Understand components, state, and props in React."
-    },
-    {
-      title: "Node.js",
-      content: "Basics of server-side development with Node.js."
-    },
-    {
-      title: "Full-Stack Development",
-      content: "Build full-stack apps with React and Node.js."
-    },
-  ];
 
   return (
     <>
@@ -44,7 +26,7 @@ export default function Home() {
         {/* Accordion */}
         <div>
           <h1 style={{ textAlign: "center" }}>Accordion</h1>
-          <Accordion items={accordianItems} />
+          <Accordion />
         </div>
         {/* Chips Input */}
         <div>
